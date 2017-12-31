d8bc4c4e738c95fc606b1258ea5abb4888a09560
Wei <weiliu1006@yahoo.com>
2017-12-31 10:56:30 +0800

    update

--- scan.py
+++ scan.py
@@ -4,22 +4,47 @@ parmela = 'wine ~/.wine/drive_c/LANL/parmela.exe '
 
 
 def getvar(filename):
-    file = open(filename, 'rb')
+    file = open(filename, 'rU')
     lines = file.readlines()
     file.close()
     mark = []
+    tempmark = []
     step = []
+    tempstep = []
     left_range = []
+    templeft_range = []
     right_range = []
+    tempright_range = []
     for line in lines:
         line = line.strip()
         var = line.split()
+        ln = len(var)
+# when open file with 'rU' doesn't work (ASCII decode doesn't work)
+# change 'rU' to 'rb' and use following loop to see which word can't decode
+#        for i in range(ln):
+#            print(var[i])
+#            var[i] = var[i].decode('ASCII')
         if var != []:
             if (var[0] == '!@var' and var[1] == '1'):
-                mark.append(var[2])
-                step.append(var[3])
-                left_range.append(var[4])
-                right_range.append(var[5])
+                tempmark.append(var[2])
+                tempstep.append(var[3])
+                templeft_range.append(var[4])
+                tempright_range.append(var[5])
+            if (var[0] == '!@subs'):
+                if(ln >= 4 and var[3] in tempmark):
+                    mark.append(var[3])
+                if(ln >= 6 and var[5] in tempmark):
+                    mark.append(var[5])
+    ln1 = len(mark)
+    ln2 = len(tempmark)
+    print(mark)
+    for i in range(ln1):
+        for j in range(ln2):
+            if tempmark[j] == mark[i]:
+                step.append(tempstep[j])
+                left_range.append(templeft_range[j])
+                right_range.append(tempright_range[j])
+                print(mark[i], step[i], left_range[i], right_range[i])
     return mark, step, left_range, right_range
 
 
@@ -37,16 +62,28 @@ def rewriteFile(filename, mark, value):
                 if(ln >= 4 and subs[3] == mark):
                     for i in range(int(subs[1])):
                         new_line = lines[k + i + 1]
-                        new_line = new_line.split(' ')
+                        new_line = new_line.split()
                         new_line[int(subs[2])] = value
-                        new_line = ' '.join(new_line)
+                        new_line = ' '.join(new_line) + '\n'
                         lines[k + i + 1] = new_line
                 if(ln >= 6 and subs[5] == mark):
                     for i in range(int(subs[1])):
                         new_line = lines[k + i + 1]
-                        new_line = new_line.split(' ')
+                        new_line = new_line.split()
                         new_line[int(subs[4])] = value
-                        new_line = ' '.join(new_line)
+                        new_line = ' '.join(new_line) + '\n'
+                        lines[k + i + 1] = new_line
+                if(ln >= 4 and subs[3] == ('-' + mark)):
+                        new_line = lines[k + i + 1]
+                        new_line = new_line.split()
+                        new_line[int(subs[2])] = ('-' + value)
+                        new_line = ' '.join(new_line) + '\n'
+                        lines[k + i + 1] = new_line
+                if(ln >= 6 and subs[5] == ('-' + mark)):
+                        new_line = lines[k + i + 1]
+                        new_line = new_line.split()
+                        new_line[int(subs[4])] = ('-' + value)
+                        new_line = ' '.join(new_line) + '\n'
                         lines[k + i + 1] = new_line
         k += 1
     file = open(filename, 'w')
@@ -91,21 +128,21 @@ def main():
         i = 0
         N = (float(right_range[i]) - float(left_range[i])) / float(step[i])
         N = int(N)
-        for j in range(N):
+        for j in range(N + 1):
             mk = mark[i]
             value = str(float(left_range[i]) + j * float(step[i]))
             rewriteFile(inputfilename, mk, value)
-            os.system(parmela + inputfilename)
+            # os.system(parmela + inputfilename)
             IsOk, goodpos = analyzeResult(outfilename, position)
             name = '_' + str(mk) + '_' + str(value)
             os.system('mv EMITTANCE.TBL EMITTANCE_' + str(name) + '.TBL')
-            os.system('mv OUTPAR.TXT OUTPAR_' + str(name) + '.TXT')
+            # os.system('mv OUTPAR.TXT OUTPAR_' + str(name) + '.TXT')
             print(value, goodpos)
             if IsOk == 1:
                 break
 
         os.system('mv EMITTANCE*.TBL ' + foldername)
-        os.system('mv OUTPAR*.TXT ' + foldername)
+        # os.system('mv OUTPAR*.TXT ' + foldername)
         os.system('cp ' + inputfilename + ' ' + foldername)
         print('done')
 
